a0aa21ba59f0d433136457c6b785ff3a2138e77a
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2023-11-12 20:32:42 +0200

    login update

--- DB_Project/FrontEnd/src/App.js
+++ DB_Project/FrontEnd/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Login from './Login'
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+
 function App()
 {
     return(
-        <div>
-            <Login />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />}></Route>
+                <Route path='/signup' element={<Signup />}></Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
