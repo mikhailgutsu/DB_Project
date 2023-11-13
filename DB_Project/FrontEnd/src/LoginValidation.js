d0c712d8663ddfb2c1a9acb513782d7d1f77ad8d
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2023-11-13 14:14:54 +0200

    FS_BD_001

--- DB_Project/FrontEnd/src/LoginValidation.js
+++ DB_Project/FrontEnd/src/LoginValidation.js
@@ -2,7 +2,7 @@ function Validation(values)
 {
     let error = {}
     const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+    const password_pattern = /^(?=.\d)(?=.[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
 
     if(values.email === "")
     {
@@ -17,13 +17,14 @@ function Validation(values)
                 error.email = ""
             }
 
+
     if(values.password === "")
     {
         error.password = "Password should not be empty"
     }
         else if(!password_pattern.test(values.password))
         {
-            error.password = "Password Didn't match"
+            error.password = "Password didn't match"
         }
             else
             {
@@ -32,5 +33,4 @@ function Validation(values)
 
     return error;
 }
-
 export default Validation;
--- DB_Project/FrontEnd/src/Signup.js
+++ DB_Project/FrontEnd/src/Signup.js
@@ -3,36 +3,38 @@ import {Link, useNavigate} from "react-router-dom";
 import Validation from './SignupValidation'
 import axios from "axios";
 
-function Signup()
-{
-    const [values, setValues] = useState({
-        name: '',
-        email: '',
-        password: ''
-    })
+function Signup() {
+    const [values, setValues] = useState
+    (
+        {
+            name: '',
+            email: '',
+            password: ''
+        })
     const navigate = useNavigate();
     const [errors, setErrors] = useState({})
-    const handleInput = (event) =>{
-        setValues(prev =>({...prev, [event.target.name]: [event.target.value]}))
+    const handleInput = (event) =>
+    {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
     }
-    const handleSubmit = (event) => {
+    const handleSubmit = (event) =>
+    {
         event.preventDefault();
-        setValues(Validation(values));
-        if(errors.name === "" && errors.email === "" && errors.password === "")
-        {
-            axios.post('http://http://localhost:8081/signup', values)
+        const err = Validation(values);  setErrors(err);
+        if(err.name === "" && err.email === "" && err.password === "") {
+            axios.post('http://localhost:8081/signup', values)
                 .then(res =>
                 {
                     navigate('/');
-                })
+                }
+                )
                 .catch(err => console.log(err));
         }
     }
-
-    return(
+    return (
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <h2>Sign Up</h2>
+                <h2>Sign-Up</h2>
                 <form action="" onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor="name"><strong>Name</strong></label>
@@ -41,9 +43,9 @@ function Signup()
                         {errors.name && <span className='text-danger'> {errors.name}</span>}
                     </div>
                     <div className='mb-3'>
-                        <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" placeholder='Enter Email' name='email'
-                               onChange={handleInput} className='form-control rounded-0'/>
+                    <label htmlFor="email"><strong>Email</strong></label>
+                    <input type="email" placeholder='Enter Email' name='email'
+                           onChange={handleInput} className='form-control rounded-0'/>
                         {errors.email && <span className='text-danger'> {errors.email}</span>}
                     </div>
                     <div className='mb-3'>
@@ -52,9 +54,9 @@ function Signup()
                                onChange={handleInput} className='form-control rounded-0'/>
                         {errors.password && <span className='text-danger'> {errors.password}</span>}
                     </div>
-                    <button type='submit' className='btn btn-success w-100'><strong>Sign Up</strong></button>
+                    <button type='submit' className='btn btn-success w-100 rounded-0'> Sign up</button>
                     <p>You are agree to our terms and policies</p>
-                    <Link to="/" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Log In</Link>
+                    <Link to="/" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Login</Link>
                 </form>
             </div>
         </div>
