7245efb9c5bb8fed7f3a9f6b108010d9a6bc3f78
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2023-11-28 13:40:38 +0300

    dev-pages

--- DB_Project/FrontEnd/src/Page/Third.js
+++ DB_Project/FrontEnd/src/Page/Third.js
@@ -30,39 +30,51 @@ function Third() {
         }
     };
 
+    const buttonStyle2 = {
+        position: 'absolute',
+        top: '390px',
+        left: '180px',
+        fontSize: '32px',
+        color: 'white', // Задает белый цвет текста
+        textShadow: '2px 2px 2px black', // Задает черную обводку (тень) текста
+    };
+
     return (
-        <div style={containerStyle}>
-            <h1>Nr. 21-30 :</h1>
-            <table style={{ color: 'white', textAlign: 'center' }}>
-                <thead>
-                <tr>
-                    <th>Nr</th>
-                    <th>Rooms</th>
-                    <th>WC</th>
-                    <th>State</th>
-                    <th>WIFI</th>
-                    <th>BED</th>
-                    <th>TV</th>
-                    <th>Balcony</th>
-                    <th>Price</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map((item) => (
-                    <tr key={item.Nr}>
-                        <td>{item.Nr}</td>
-                        <td>{item.Rooms}</td>
-                        <td>{item.WC}</td>
-                        <td>{item.State}</td>
-                        <td>{item.WIFI}</td>
-                        <td>{item.BED}</td>
-                        <td>{item.TV}</td>
-                        <td>{item.Balcony}</td>
-                        <td>{item.Price}</td>
+        <div>
+            <div style={containerStyle}>
+                <h1>Nr. 21-30 :</h1>
+                <table style={{ color: 'white', textAlign: 'center' }}>
+                    <thead>
+                    <tr>
+                        <th>Nr</th>
+                        <th>Rooms</th>
+                        <th>WC</th>
+                        <th>State</th>
+                        <th>WIFI</th>
+                        <th>BED</th>
+                        <th>TV</th>
+                        <th>Balcony</th>
+                        <th>Price</th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {data.map((item) => (
+                        <tr key={item.Nr}>
+                            <td>{item.Nr}</td>
+                            <td>{item.Rooms}</td>
+                            <td>{item.WC}</td>
+                            <td>{item.State}</td>
+                            <td>{item.WIFI}</td>
+                            <td>{item.BED}</td>
+                            <td>{item.TV}</td>
+                            <td>{item.Balcony}</td>
+                            <td>{item.Price}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+            <Link to="/Page/Second" className='btn btn-default text-decoration-none' style={buttonStyle2}> Back </Link>
         </div>
     );
 }
