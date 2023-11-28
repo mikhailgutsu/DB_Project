7f3766c5b411264252ef0e59391663d9dd01c468
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2023-11-28 12:21:39 +0300

    lvl-update

--- DB_Project/FrontEnd/src/Page/Second.js
+++ DB_Project/FrontEnd/src/Page/Second.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import pickImage from "./assets/second.jpg";
 
 function Second() {
+    const [data, setData] = useState([]);
     const containerStyle = {
         background: `url(${pickImage})`,
         backgroundSize: 'cover',
         width: '100%',
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center', // Центрировать содержимое по горизонтали
-        alignItems: 'center', // Центрировать содержимое по вертикали
-        flexDirection: 'column', // Разместить элементы вертикально
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        color: 'white', // Добавил белый цвет текста для читаемости на фоне изображения
     };
 
-    return(
-        <div style={containerStyle}>
+    useEffect(() => {
+        fetchData();
+    }, []);
 
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8081/getdata');
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    return (
+        <div style={containerStyle}>
+            <h1>Data from MySQL Table</h1>
+            <table border="1" style={{ color: 'white', textAlign: 'center' }}>
+                <thead>
+                <tr>
+                    <th>Nr</th>
+                    <th>Rooms</th>
+                    <th>WC</th>
+                    <th>State</th>
+                    <th>WIFI</th>
+                    <th>BED</th>
+                    <th>TV</th>
+                    <th>Balcony</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((item) => (
+                    <tr key={item.Nr}>
+                        <td>{item.Nr}</td>
+                        <td>{item.Rooms}</td>
+                        <td>{item.WC}</td>
+                        <td>{item.State}</td>
+                        <td>{item.WIFI}</td>
+                        <td>{item.BED}</td>
+                        <td>{item.TV}</td>
+                        <td>{item.Balcony}</td>
+                        <td>{item.Price}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 export default Second;
