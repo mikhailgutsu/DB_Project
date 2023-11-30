5093ac297391dfa24552d812af43c9f6ad20e484
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2023-11-30 14:53:08 +0300

    +5

---
+++ DB_Project/FrontEnd/src/Page/Rooms/five.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+
+function Five()
+{
+
+
+    return(
+        <div>
+
+
+        </div>
+    );
+
+}
+export default Five;
---
+++ DB_Project/FrontEnd/src/Page/Rooms/scnd.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+
+function Scnd()
+{
+
+
+    return(
+        <div>
+
+
+        </div>
+    );
+
+}
+export default Scnd;
