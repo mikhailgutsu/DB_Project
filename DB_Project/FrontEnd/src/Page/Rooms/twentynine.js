5e69b0404aa75439ba93e3c8328655d2e38d5448
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2023-12-01 16:15:49 +0300

    rooms-dev

--- DB_Project/FrontEnd/src/Page/Rooms/twentynine.js
+++ DB_Project/FrontEnd/src/Page/Rooms/twentynine.js
@@ -1,15 +1,28 @@
 import React, {useEffect, useState} from 'react';
+import pickImage from "./assets/abc.jpg";
 
 function Twentynine()
 {
 
 
+    const containerStyle = {
+        background: `url(${pickImage})`,
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        color: 'white', // Добавил белый цвет текста для читаемости на фоне изображения
+    };
+
     return(
         <div>
+            <div style={containerStyle}>
 
-
+            </div>
         </div>
     );
-
 }
 export default Twentynine;
--- DB_Project/FrontEnd/src/Page/Rooms/twentysix.js
+++ DB_Project/FrontEnd/src/Page/Rooms/twentysix.js
@@ -1,13 +1,27 @@
 import React, {useEffect, useState} from 'react';
+import pickImage from "./assets/abc.jpg";
 
 function Twentysix()
 {
 
 
+    const containerStyle = {
+        background: `url(${pickImage})`,
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        color: 'white', // Добавил белый цвет текста для читаемости на фоне изображения
+    };
+
     return(
         <div>
+            <div style={containerStyle}>
 
-
+            </div>
         </div>
     );
 
